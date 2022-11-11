bbb1d63ffa7be38180c1931dc66970db310684d5
KaterynaShelest <shelestkateryna1994@gmail.com>
2022-11-11 09:21:45 +0200

    js Home Task 4 full

--- js-ht4/main.js
+++ js-ht4/main.js
@@ -55,46 +55,40 @@ while (n<20) {
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 //
 let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-for (let item of listOfItems) {
-    document.write(`<ul>
-       <li>html</li>
-       <li>css</li>
-       <li>javascript</li>
-       <li>mongodb</li>
-       <li>react</li>
-       <li>angular</li>
-       <li>node.js</li>
-</ul>
-`)
-}
 
+document.write(`<ul>`)
+for (const listOfItem of listOfItems) {
+    document.write(`<li>${listOfItem}</li>`)
+
+}
+document.write(`</ul>`)
 
 //     Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону  Зробити адекватну стилізацію
 // Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
 //
-// let products = [
-//     {
-//         title: 'milk',
-//         price: 22,
-//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-//     },
-//     {
-//         title: 'juice',
-//         price: 27,
-//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-//     },
-//     {
-//         title: 'tomato',
-//         price: 47,
-//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-//     },
-//     {
-//         title: 'tea',
-//         price: 15,
-//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-//     },
-// ];
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
 //
 // ШАБЛОН
 // <div class="product-card">
@@ -106,21 +100,47 @@ for (let item of listOfItems) {
 //
 //
 // --------------------
+for (const product of products) {
+    document.write(`
+ <div class="product-card">
+    <h3 class="product-title">TITLE ${product.title}. Price ${product.price}</h3>
+    <img src="${product.image}" alt="${product.title}" class="product-image">
+</div>
+`)
+}
 //     є масив
-// let users = [
-//     {name: 'vasya', age: 31, status: false},
-//     {name: 'petya', age: 30, status: true},
-//     {name: 'kolya', age: 29, status: true},
-//     {name: 'olya', age: 28, status: false},
-//     {name: 'max', age: 30, status: true},
-//     {name: 'anya', age: 31, status: false},
-//     {name: 'oleg', age: 28, status: false},
-//     {name: 'andrey', age: 29, status: true},
-//     {name: 'masha', age: 30, status: true},
-//     {name: 'olya', age: 31, status: false},
-//     {name: 'max', age: 31, status: true}
-// ];
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
 // за допомоги циклу вивести:
 //     - користувачів зі статусом true
+for (const user of users) {
+    if(user.status) {
+        console.log(user)
+    }
+
+}
 // - користувачів зі статусом false
+for (const user of users) {
+    if (!user.status) {
+        console.log(user)
+    }
+
+}
 // - користувачів які старші за 30 років
+for (const user of users) {
+    if (user.age > 30) {
+        console.log(user)
+    }
+
+}
